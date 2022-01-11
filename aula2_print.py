b7ab1470f409ee57fd34767c4990888ed111edf5
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2022-01-11 19:15:06 -0300

    aula2_print

---
+++ aula2_print.py
@@ -0,0 +1,13 @@
+print(123456)
+# usando o separador sep
+print('João', 'e', 'Maria', sep='-')
+# usando argumento end para o final de frase
+print('João', 'e', end=' ####### ')
+print('Maria')
+print('João Fernando')
+print("String ou 'str' são tipos de textos")
+print('String ou "str" são tipos de textos')
+""" exercícios digitar um número de CPF
+usando os comandos sep e end """
+print('824', '179', '568', sep='.', end='-')
+print('18')
