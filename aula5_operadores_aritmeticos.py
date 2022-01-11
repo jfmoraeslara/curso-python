87c955d7be1e6f2c9dab278fab94fb323e587753
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2022-01-11 19:17:24 -0300

    aula5_operadores_aritmeticos

---
+++ aula5_operadores_aritmeticos.py
@@ -0,0 +1,14 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+print("Multpicação 10 * 10 = ", 10 * 10)
+print("Adição 10 + 10 = ", 10 + 10)
+print("Subtração 10 - 5 = ", 10 - 5)
+print("Divisão 10 / 2 = ", 10 / 2)
+print("Repetição 3 * João: ", 3 * 'João ')
+print("Contatenação 5 mais 5 = ", '5' + '5')
+print("João" + " " + "Fernando" + " tem " + str(40) + " anos.")
+print("Divisão inteira de 10.3 // 5 = ", 10.3 // 5)
+print("Resto da divisão = 10 % 3 = ", 10 % 3)
+
+
